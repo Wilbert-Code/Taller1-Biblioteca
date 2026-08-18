8f2d282a601faebe76da549da686aff7f3bb8296
wilberto <warizaz@unicesr.edu.co>
2026-08-18 10:52:06 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/taller1/biblioteca/Cliente.java
+++ src/main/java/com/mycompany/taller1/biblioteca/Cliente.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.taller1.biblioteca;
 
-/**
- *
- * @author ESTUDIANTES
- */
-public class Cliente {
-    
+public class Cliente extends Persona {
+    private String email;
+
+    public Cliente() {
+        super();
+    }
+
+    public Cliente(String id, String nombre, String telefono, String email) {
+        super(id, nombre, telefono);
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Email: " + email;
+    }
 }
--- src/main/java/com/mycompany/taller1/biblioteca/Main.java
+++ src/main/java/com/mycompany/taller1/biblioteca/Main.java
@@ -1,16 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.taller1.biblioteca;
 
-/**
- *
- * @author ESTUDIANTES
- */
+import java.util.ArrayList; 
+import java.util.Scanner; 
+
+
 public class Main {
+    
+      
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Sistema de Biblioteca - Gestión de Clientes");
+    } 
+    
+
+
+    public static void crearCliente() {
+        System.out.println("\n--- CREAR CLIENTE ---");
+        System.out.print("ID: ");
+        String id = sc.nextLine();
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("Teléfono: ");
+        String telefono = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+            Cliente nuevo = new Cliente(id, nombre, telefono, email);
+        clientes.add(nuevo);
+        System.out.println("Cliente registrado exitosamente.");
+        }
     }
-}
